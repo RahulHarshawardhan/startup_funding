4d307d853509716684dce23287989c80b94af027
Rahul <rahulharshawardhan@gmail.com>
2023-03-10 00:07:08 +0530

    biggest investment part completed

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout='wide', page_title='startUp Analysis')
 
 df = pd.read_csv("startup_cleaned.csv")
+
+def load_investor_details(investor):
+    st.title(investor)
+    #Load the recent 5 investments of the investor
+    last5_df = df[df['investors'].str.contains(investor)].head()[['date', 'startup', 'vertical', 'city', 'round', 'amount']]
+    st.subheader("Most Recent Investments")
+    st.dataframe(last5_df)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        #biggest investments
+        big_series = df[df['investors'].str.contains(investor)].groupby('startup')['amount'].sum().sort_values(ascending = False).head()
+        st.subheader("Biggest Investments")
+        fig, ax = plt.subplots()
+        ax.bar(big_series.index, big_series.values)
+        st.pyplot(fig)
+
+
+
+    #st.dataframe(big_series)
+
+
 
 
 
@@ -19,8 +44,11 @@ elif option == 'StartUP':
 
 
 else:
-    st.sidebar.selectbox('Select StartUP', sorted(set(df['investors'].str.split(',').sum())))
+    selected_investor = st.sidebar.selectbox('Select StartUP', sorted(set(df['investors'].str.split(',').sum())))
     btn2 = st.sidebar.button('Find Investor Details')
-    st.title('Investor Analysis')
+    if btn2:
+        load_investor_details(selected_investor)
+
+
 
 
