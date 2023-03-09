bca4fe0e7b0a132590ed6ae4ec493c8aeb78380a
Rahul <rahulharshawardhan@gmail.com>
2023-03-09 23:13:46 +0530

    Dataset changed to startup_cleaned

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("startup_funding.csv")
+df = pd.read_csv("startup_cleaned.csv")
 
-#data cleaning
-df['Investors Name']= df["Investors Name"].fillna('Undisclosed')
+
 
 st.sidebar.title("Startup Funding Analysis")
 
-option =st.sidebar.selectbox('Select One', ['Overall Analysis', 'StartUP', 'Investors'])
+option = st.sidebar.selectbox('Select One', ['Overall Analysis', 'StartUP', 'Investors'])
 
 if option == "Overall Analysis":
     st.title('Overall Analysis')
 
 elif option == 'StartUP':
-    st.sidebar.selectbox('Select StartUP', sorted(df['Startup Name'].unique().tolist()))
+    st.sidebar.selectbox('Select StartUP', sorted(df['startup'].unique().tolist()))
     btn1 = st.sidebar.button("Find startup Details ")
     st.title('StartUP Analysis')
 
 
 else:
-    st.sidebar.selectbox('Select StartUP', sorted(df['Investors Name'].unique().tolist()))
+    st.sidebar.selectbox('Select StartUP', sorted(set(df['investors'].str.split(',').sum())))
     btn2 = st.sidebar.button('Find Investor Details')
     st.title('Investor Analysis')
 
