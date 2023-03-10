ccfa98e07d34ad025eb64d193edffbfaf9d1fb2d
Rahul <rahulharshawardhan@gmail.com>
2023-03-10 10:12:51 +0530

    col created for the sector invested in

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ def load_investor_details(investor):
         fig, ax = plt.subplots()
         ax.bar(big_series.index, big_series.values)
         st.pyplot(fig)
+    with col2:
+        vertical_series = df[df['investors'].str.contains(investor)].groupby('vertical')['amount'].sum()
+        st.subheader('Sectors Invested in')
+        fig1, ax1 = plt.subplots()
+        ax1.pie(vertical_series, labels=vertical_series, autopct="%0.01f%%")
+        st.pyplot(fig1)
+
+        
+
 
 
 
